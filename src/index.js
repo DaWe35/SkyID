7f534eba4e5f7fb1257796e7dd22bab1d72270fb
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-10-20 17:03:40 +0200

    use Sia-js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-var blake = require('blakejs')
-const nacl = require('tweetnacl')
+var sia = require('sia-js')
 global.Buffer = global.Buffer || require('buffer').Buffer
 
 
-function hasStrongRandom() {
-	return 'crypto' in window && window['crypto'] !== null
-}
 
 function toHexString(byteArray) {
 	return Array.from(byteArray, function(byte) {
@@ -13,37 +9,21 @@ function toHexString(byteArray) {
 	}).join('')
 }
 
-/* var mnemonics = { "english": new Mnemonic("english") }
-var mnemonic = mnemonics["english"] */
+sia.keyPair.generateRandomData(randomCallback)
 
-if (!hasStrongRandom()) {
-	var errorText = "This browser does not support strong randomness"
-	alert(errorText)
-	// return
+function randomCallback(random_data) {
+	console.log('random data:', random_data)
+
+	var nonce = Buffer.from([0,0,0,0,0,0,0,0])
+	var arr = [random_data, nonce]
+	
+	var seed = Buffer.concat(arr)
+	console.log('seed:', seed)
+	// show the words
+	/* var words = mnemonic.toMnemonic(data)
+	
+	console.log(data) */
+	let address = sia.keyPair.generateFromSeed(seed)
+	console.log('private key:', address)
 }
-
-// get the amount of entropy to use
-var numWords = 24
-var strength = numWords / 3 * 32
-var buffer = new Uint8Array(strength / 8) // 32
-// create secure entropy
-var random_data = crypto.getRandomValues(buffer)
-
-var nonce = Buffer.from([0,0,0,0,0,0,0,0])
-var arr = [random_data, nonce]
-
-var seed = Buffer.concat(arr)
-console.log(seed)
-// show the words
-/* var words = mnemonic.toMnemonic(data)
-
-console.log(data) */
-
-hash = blake.blake2b(seed, false, 32)
-
-specifier = Buffer.from([101,100,50,53,53,49,57,0,0,0,0,0,0,0,0,0])
-keypair = nacl.sign.keyPair.fromSeed(hash)
-pubkey = toHexString(Buffer.concat([specifier, keypair['publicKey']]))
-console.log(pubkey)
-console.log(keypair['publicKey'])
 
