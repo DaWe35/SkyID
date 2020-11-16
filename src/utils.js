a5ab73f8d7a9ed492cfc8223c81a6f9270b9858c
10000multiplier <57633368+10000multiplier@users.noreply.github.com>
2020-11-16 12:04:02 -0500

    new subtle spinner

--- src/utils.js
+++ src/utils.js
@@ -182,81 +182,19 @@ export function showOverlay(opts) {
 		return
 	}
 
-	// multiple loading animations from https://codepen.io/AlexWarnes/pen/jXYYKL - random displayed
-	let loadScreens = [
-		`<!-- GRADIENT SPINNER -->
-		<div class="spinner-box">
-			<div class="circle-border">
-				<div class="circle-core"></div>
-			</div>
-		</div>`,
 
-		`<!-- SPINNER ORBITS -->
-		<div class="spinner-box">
-			<div class="blue-orbit leo"></div>
-			<div class="green-orbit leo"></div>
-			<div class="red-orbit leo"></div>
-			<div class="white-orbit w1 leo"></div>
-			<div class="white-orbit w2 leo"></div>
-			<div class="white-orbit w3 leo"></div>
-		</div>`,
-
-		`<!-- GRADIENT CIRCLE PLANES -->
-		<div class="spinner-box">
-			<div class="leo-border-1">
-				<div class="leo-core-1"></div>
-			</div>
-			<div class="leo-border-2">
-				<div class="leo-core-2"></div>
-			</div>
-		</div>`,
-
-		`<!-- SPINNING SQUARES -->
-		<div class="spinner-box">
-			<div class="configure-border-1">
-				<div class="configure-core"></div>
-			</div>
-			<div class="configure-border-2">
-				<div class="configure-core"></div>
-			</div>
-		</div>`,
-
-		`<!-- LOADING DOTS... -->
-		<div class="spinner-box">
-			<div class="pulse-container">
-				<div class="pulse-bubble pulse-bubble-1"></div>
-				<div class="pulse-bubble pulse-bubble-2"></div>
-				<div class="pulse-bubble pulse-bubble-3"></div>
-			</div>
-		</div>`,
-
-		`<!-- SOLAR SYSTEM -->
-		<div class="spinner-box">
-			<div class="solar-system">
-				<div class="earth-orbit orbit">
-					<div class="planet earth"></div>
-					<div class="venus-orbit orbit">
-						<div class="planet venus"></div>
-							<div class="mercury-orbit orbit">
-								<div class="planet mercury"></div>
-							<div class="sun"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>`,
-
+	let spinner =
 		`<!-- Three Quarter Spinner -->
 		<div class="spinner-box">
 		  	<div class="three-quarter-spinner"></div>
-		</div>`,
-	]
+		</div>`
+
 
 
 	if (!document.getElementById('loadingOverlay')) {
 		var loadScreen = document.createElement("div")
 		loadScreen.id = 'loadingOverlay'
-		loadScreen.innerHTML = loadScreens[Math.floor(Math.random() * loadScreens.length)]
+		loadScreen.innerHTML = spinner
 		document.body.appendChild(loadScreen)
 	}
 }
