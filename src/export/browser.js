758a9aa94cb0f42e5bf6450b89684d94c8b32473
Marcin S <marcin@bytedude.com>
2021-04-29 16:33:50 -0500

    Fix handshake and remove username stuff

--- src/export/browser.js
+++ src/export/browser.js
@@ -1,3 +1,7 @@
+import { WindowMessenger, ChildHandshake } from "post-me";
 import { SkyID } from '../skyid'
 
 window.SkyID = SkyID
+
+window.WindowMessenger = WindowMessenger;
+window.ChildHandshake = ChildHandshake;
